ad790b64b40af8e36257569a86af3233355ef922
gillesFontaineVives <146202087+gillesFontaineVives@users.noreply.github.com>
2024-02-28 17:13:09 +0100

    Labo3-2

--- Semester2/Labo3/Fibonnanci/scripts/index.js
+++ Semester2/Labo3/Fibonnanci/scripts/index.js
@@ -1,8 +1,32 @@
 const setup = () => {
-    let getal = 2;
-    for (let i = 1; i < 1000 ; i++) {
-        
+    let getal = 3;
+    let getal1 = 0;
+    let som = 0;
+    for (let i = 1; i < 4000000 ;i) {
+        if (i === 1) {
+            i = i + i;
+            if(i % 2 === 0){
+                som += i;
+            }
+            getal1 = i;
+        } else {
+            if (getal > getal1) {
+                i = getal1 + getal;
+                if(i % 2 === 0){
+                    som += i;
+                }
+                getal1 = i;
+            } else {
+                i = getal + getal1;
+                if(i % 2 === 0){
+                    som += i;
+                }
+                getal = i;
+            }
+        }
     }
+    console.log(som);
+
 }
 
 window.addEventListener("load", setup);
