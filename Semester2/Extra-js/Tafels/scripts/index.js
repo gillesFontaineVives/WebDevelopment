3d3ae93d0fb67ab42d383165cac391ac8390c079
gillesFontaineVives <146202087+gillesFontaineVives@users.noreply.github.com>
2024-03-18 16:22:47 +0100

    Extra-js_V2

--- Semester2/Extra-js/Tafels/scripts/index.js
+++ Semester2/Extra-js/Tafels/scripts/index.js
@@ -4,14 +4,42 @@ const setup = () => {
 }
 
 const tafelsrekenen = () => {
-	let tafeltekst = document.getElementById("tafel");
+	verwijderAlleChildren(div);
+	let tafeltekst = document.getElementById("tafel").value;
 	let error = document.getElementById("errorMessage");
-	if(isGetal(tafeltekst.value)){
-		
+
+	let tafelTabel = document.createElement("div");
+	tafelTabel.className = "Tafeltabel";
+
+	var datum = new Date();
+	var tijd = datum.getHours() + ":" + datum.getMinutes() + ":" + datum.getSeconds();
+
+	if(isGetal(tafeltekst) && tafeltekst.length !== 0){
+		let div = document.createElement("div");
+		div.className = "header";
+		div.innerHTML = "Tafel van " + tafeltekst + " aangemaakt op " + tijd + "\n";
+		tafelTabel.appendChild(div);
+
+		for (let i = 1; i < 11; i++) {
+			let number = tafeltekst * i;
+			div = document.createElement("div");
+			div.className = "divspec";
+			div.innerHTML += tafeltekst + " x " + i + " = " +  number + "\n";
+			tafelTabel.appendChild(div);
+		}
+
+		document.body.appendChild(tafelTabel);
 	}else{
 		error.innerHTML = "Geef een nummer";
 	}
 }
+
+const verwijderAlleChildren = (element) => {
+	while (element.firstChild) {
+		element.removeChild(element.firstChild);
+	}
+}
+
 
 const isGetal = (tekst) => {
 	return !isNaN(tekst);
