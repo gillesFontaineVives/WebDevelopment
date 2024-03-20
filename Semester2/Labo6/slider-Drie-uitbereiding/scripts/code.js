9562e35d2ba6cb6def963c5a831bbb3a87905275
gillesFontaineVives <146202087+gillesFontaineVives@users.noreply.github.com>
2024-03-20 19:12:52 +0100

    Labo6_V3

--- Semester2/Labo6/slider-Drie-uitbereiding/scripts/code.js
+++ Semester2/Labo6/slider-Drie-uitbereiding/scripts/code.js
@@ -22,11 +22,14 @@ const savecolor = () => {
 	buttonkleur.setAttribute("data-rood", sliders[0].value);
 	buttonkleur.setAttribute("data-groen", sliders[1].value);
 	buttonkleur.setAttribute("data-blauw", sliders[2].value);
+	buttonkleur.setAttribute("data-doorz", sliders[3].value/10);
 	buttonkleur.addEventListener("click", restore);
 	buttonkleur.style.backgroundColor = kleur;
-	//buttonkleur.className = "colorDemo";
+	buttonkleur.className = "colorDemo-klein";
 
 	let buttondelete = document.createElement("button");
+	buttondelete.addEventListener("click", deletebutton)
+	buttondelete.classList = "delete";
 	buttondelete.textContent = "X";
 	buttonkleur.appendChild(buttondelete);
 	section.appendChild(buttonkleur);
@@ -41,23 +44,40 @@ const update = () => {
 	let Roodvalue=sliders[0].value;
 	let Groenvalue=sliders[1].value;
 	let Blauwvalue=sliders[2].value;
-	//let doorzicht=sliders[3].value / 10;
+	let doorzicht=sliders[3].value / 10;
 
 	values[0].innerHTML = Roodvalue;
 	values[1].innerHTML = Groenvalue;
 	values[2].innerHTML = Blauwvalue;
-	//values[3].innerHTML= doorzicht.toString();
+	values[3].innerHTML= doorzicht.toString();
 
-	colorDemos[0].style.backgroundColor= 'rgba(' + Roodvalue + ',' + Groenvalue + ',' + Blauwvalue + ')';
-	//colorDemos[0].style.backgroundColor= 'rgba(' + Roodvalue + ',' + Groenvalue + ',' + Blauwvalue + ',' + doorzicht + ')';
+	colorDemos[0].style.backgroundColor= 'rgba(' + Roodvalue + ',' + Groenvalue + ',' + Blauwvalue + ',' + doorzicht + ')';
+}
+
+const deletebutton = (event) => {
+	event.stopPropagation();
+	let section = document.querySelector("section");
+
+	let button = event.target.parentElement;
+	section.removeChild(button);
 }
 
 const restore = (event) => {
+	event.stopPropagation()
 	let colorDemos=document.getElementsByClassName("colorDemo");
-	let Roodvalue= event.target;
-	let Groenvalue=sliders[1].value;
-	let Blauwvalue=sliders[2].value;
-	colorDemos[0].style.backgroundColor = 'rgba(' + Roodvalue + ',' + Groenvalue + ',' + Blauwvalue + ')';
+	let sliders = document.getElementsByClassName("slider")
+
+	let Roodvalue=event.target.getAttribute("data-rood");
+	let Groenvalue=event.target.getAttribute("data-groen");
+	let Blauwvalue=event.target.getAttribute("data-blauw");
+	let doorzicht=event.target.getAttribute("data-doorz");
+
+	sliders[0].value = Roodvalue;
+	sliders[1].value = Groenvalue;
+	sliders[2].value = Blauwvalue;
+	sliders[3].value = doorzicht*10;
+
+	colorDemos[0].style.backgroundColor= 'rgba(' + Roodvalue + ',' + Groenvalue + ',' + Blauwvalue + ',' + doorzicht + ')';
 }
 
 window.addEventListener("load", setup);
